b254e0cbe911fee6a228787de3039241e92692a8
borjabordel <borjabordel@users.noreply.github.com>
2018-02-27 17:25:18 +0100

    ZigBee INICIO

---
+++ ZigBee/Antena/antenaInit.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+# Escaneo ZigBee
+
+# Código de recepción en las antenas para la tecnología ZigBee
+# PERIMETER SECURITY
+# 2017
+# Borja Bordel GISAI UPM
+
+
+java -jar discover.jar

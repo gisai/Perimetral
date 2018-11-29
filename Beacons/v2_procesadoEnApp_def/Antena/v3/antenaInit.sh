45fd530edd019d297aa68a6f211b4764a429cfc3
borjabordel <borjabordel@users.noreply.github.com>
2018-11-29 12:47:55 +0100

    Versión beacon EC-Innova

---
+++ Beacons/v2_procesadoEnApp_def/Antena/v3/antenaInit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Código de inicialización de antenas Beacon
+# PERIMETER SECURITY
+# 2017
+# Borja Bordel GISAI UPM
+
+#IMPORTANTE Para poder aplicar la conexión USB esta antena debe
+#tener activado el servidor ADB
+
+#systemctl start adbd.service
+#systemctl enable adbd.service
+
+#Reiniciamos todos los módulos Bluetooth por si hubiera algún 
+#sistema caído
+service bluetooth restart
+service dbus restart 
+hciconfig hci0 reset 
+
+root/beaconAntena.sh -b
